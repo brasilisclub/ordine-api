8172c3198dd1f2cb001db23b7766edcac2b61d1f
brasilis <matheushpr9@gmail.com>
2024-12-01 21:53:36 -0300

    skeleton of project

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"ordine-api/pkg/routes"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	routes.ProductRoutes(r)
+
+	r.Run(":8080") // listen and serve on 0.0.0.0:8080
+}
