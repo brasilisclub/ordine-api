6b88db4620d680fb9638a7776e1216ca002d0788
brasilis <matheushpr9@gmail.com>
2024-12-04 23:42:56 -0300

    Singleton implementation in GetConnector

--- pkg/database/get_connector.go
+++ pkg/database/get_connector.go
@@ -1,12 +1,28 @@
 package database
 
 import (
+	"sync"
+
+	"github.com/sirupsen/logrus"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
+var (
+	instance *gorm.DB
+	once     sync.Once
+	err      error
+)
+
 func GetConnector() (*gorm.DB, error) {
-	dsn := "root:root@tcp(database:3306)/ordine?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{}) // fazer singleton
-	return db, err
+	once.Do(func() {
+		dsn := "root:root@tcp(database:3306)/ordine?charset=utf8mb4&parseTime=True&loc=Local"
+		instance, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		if err != nil {
+			return
+		}
+		logrus.Info("Database connection successfully established!")
+	})
+
+	return instance, err
 }
