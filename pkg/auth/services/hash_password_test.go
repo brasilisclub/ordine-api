53df80d51b59a0d6b642951022e91689ffdcf94c
brasilis <matheushpr9@gmail.com>
2025-01-27 21:47:54 -0300

    100 cover on get hash password service

---
+++ pkg/auth/services/hash_password_test.go
@@ -0,0 +1,40 @@
+package services
+
+import "testing"
+
+func TestHashPassword(t *testing.T) {
+	type args struct {
+		password string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "Should error, password > 72",
+			args: args{
+				password: "Password longer than 72 characters.......................................",
+			},
+			wantErr: true,
+		},
+		{
+			name: "Should execute without errors",
+			args: args{
+				password: "test",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := HashPassword(tt.args.password)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("HashPassword() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+		})
+	}
+}
